b88d5a8f6efcbdd00bd45f96156f34a5c263a4fe
JScearcy <jacobscearcy@gmail.com>
2016-09-11 21:51:34 -0500

    additional work on app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,37 @@
 import {Component} from "@angular/core";
+import {User} from "./shared/user/user";
 
 @Component({
   selector: "my-app",
-  template: "<Label text='hello world'></Label>"
+  template: "pages/login/login.html",
+  styleUrls: [ "pages/login/login-common.css", "pages/login/login.css" ]
 })
-export class AppComponent {}
+export class AppComponent {
+  private user: User;
+  private isLoggingIn: boolean = true;
+
+  constructor() {
+    this.user = new User();
+  }
+
+  submit() {
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+
+  login() {
+    alert("Login");
+  }
+
+  signUp() {
+    alert("signup");
+    //this._userService.register(this.user);
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
